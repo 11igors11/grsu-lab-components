f1206d93b3fc09ef8c2771f2d9471f8e14c935c9
Moonlight Walker <11igors11@gmail.com>
2016-10-12 11:50:08 +0300

    add multiple components support

---
+++ form2/form2.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+class CustomForm extends Component {
+ 
+  render() {
+    return (
+      <div>
+        form2
+      </div>
+    );
+  }
+}
+
+export default CustomForm;
